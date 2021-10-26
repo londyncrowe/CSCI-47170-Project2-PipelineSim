299ec8c146037dfd8f94b7109221b0154257144a
Jay3420 <fieldsja1@etsu.edu>
2021-10-26 15:26:40 -0400

    comments

--- Project2/Simulate.cs
+++ Project2/Simulate.cs
@@ -8,21 +8,28 @@ namespace Project2
 {
     class Simulate
     {
+        //private latency values for all the ops, could possible be removed if we added a config file and passed
+        //in all of our latency values in the config file
         private static int lw = 1, flw = 1, sw = 1, fsw = 1, add = 1, sub = 1, bne = 1, beq = 1, fadd = 1,
             fsub = 1, fmul = 4, fdiv = 9, write = 1, read = 1, commit = 1, issue = 1;
         public static void Sim(List<string> instructions)
         {
+            //used for print formatting in the switch cases
             string strIssue, strExecute, strRead, strWrite, strCommit;
-            int count = 1;
+            int count = 0;
             Decode decode = new Decode();
+            //loop through all the instructions
             for(int i = 0; i < instructions.Count(); i++)
             {
+                //use decode to grab the operation we will be performing
                 List<string> decodedInstruction = decode.DecodeInstruction(instructions[i]);
 
+                //first position of the decodedInstruction list will have the instruction
                 switch(decodedInstruction[0])
                 {
                     #region lwCase
                     case "lw":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -40,6 +47,7 @@ namespace Project2
                     #endregion
                     #region flwCase
                     case "flw":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -57,6 +65,7 @@ namespace Project2
                     #endregion
                     #region swCase
                     case "sw":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -74,6 +83,7 @@ namespace Project2
                     #endregion
                     #region fswCase
                     case "fsw":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -91,6 +101,7 @@ namespace Project2
                     #endregion
                     #region addCase
                     case "add":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -107,6 +118,7 @@ namespace Project2
                     #endregion
                     #region subCase
                     case "sub":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -123,6 +135,7 @@ namespace Project2
                     #endregion
                     #region beqCase
                     case "beq":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -139,6 +152,7 @@ namespace Project2
                     #endregion
                     #region bneCase
                     case "bne":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -155,6 +169,7 @@ namespace Project2
                     #endregion
                     #region faddCase
                     case "fadd.s":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -171,6 +186,7 @@ namespace Project2
                     #endregion
                     #region fsubCase
                     case "fsub.s":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -187,6 +203,7 @@ namespace Project2
                     #endregion
                     #region fmulCase
                     case "fmul.s":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
@@ -203,6 +220,7 @@ namespace Project2
                     #endregion
                     #region fdivCase
                     case "fdiv.s":
+                        count++;
                         strIssue = count.ToString();
                         count += issue;
                         strExecute = count.ToString() + " - ";
